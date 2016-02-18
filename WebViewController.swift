c0095518df0a1a423c910fc57e1d013d04306591
DanielORorke <dororke@dororke-mac.local>
2016-02-17 21:55:12 -0800

    Implemented WebVC & "FinishedLoad" override
    Attempting to use a WebViewController and DidFinishLoading override to
    capture the URLs when the webView finishes loading. Then capturing the
    AuthCode form the URL.

---
+++ WebViewController.swift
@@ -0,0 +1,64 @@
+//
+//  WebViewController.swift
+//  OAuthHack
+//
+//  Created by Daniel O'Rorke on 2/17/16.
+//  Copyright © 2016 Aerohive Networks. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set UIWebView Delegate!!
+
+    @IBOutlet weak var myWebView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    //======= THIS GENERATES THE WEBVIEW WHEN USERS LOG IN=========
+        // MARK: Variables for OAuth
+        let clientID = "19a087a8"
+        let clientSecret = "daffa2ecd066ef09da98e4527749dee2"
+        let redirectURL = "https://developer.aerohive.com/"
+        
+        // Set up the OAuth URL to call
+        let authQueryParams = "?client_id="+clientID+"&redirect_uri="+redirectURL
+        let urlString = "https://cloud.aerohive.com/thirdpartylogin"+authQueryParams
+        let url = NSURL (string: urlString)
+        
+        let requestObj = NSURLRequest(URL: url!);
+        myWebView.delegate = self // Allows us to OVERRIDE functions
+        myWebView.loadRequest(requestObj);
+    //=============================================================
+    }
+
+    func webViewDidFinishLoad(webView : UIWebView) {
+        print("Finished!")
+        let requestedURL = myWebView.request?.URL
+        print (requestedURL)
+        
+    }
+    func webViewDidStartLoad(webView: UIWebView) {
+        print("Started")
+        let requestedURL = myWebView.request?.URL
+        print (requestedURL)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

42da12a864f46227253adc7b660b8d6744d1a2af
DanielORorke <dororke@dororke-mac.local>
2016-02-18 16:06:05 -0800

    Moved paramsFromQueryString to the WebVC
    Moved some functions from the App Delegate to the WebVC

--- WebViewController.swift
+++ WebViewController.swift
@@ -5,10 +5,17 @@
 //  Created by Daniel O'Rorke on 2/17/16.
 //  Copyright © 2016 Aerohive Networks. All rights reserved.
 //
+//  Special thanks to Renjie (Rodger) Wang for the idea.
+//
 
 import UIKit
 
 class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set UIWebView Delegate!!
+    
+    // MARK: Variables for OAuth
+    let clientID = "52739d49"
+    let clientSecret = "069881278521632ab86c6ed946629dd1"
+    let redirectURL = "https://developer.aerohive.com"
 
     @IBOutlet weak var myWebView: UIWebView!
 
@@ -18,10 +25,7 @@ class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set
         // Do any additional setup after loading the view.
         
     //======= THIS GENERATES THE WEBVIEW WHEN USERS LOG IN=========
-        // MARK: Variables for OAuth
-        let clientID = "52739d49"
-        let clientSecret = "069881278521632ab86c6ed946629dd1"
-        let redirectURL = "https://developer.aerohive.com"
+
         
         // Set up the OAuth URL to call
         let authQueryParams = "?client_id="+clientID+"&redirect_uri="+redirectURL
@@ -36,8 +40,19 @@ class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set
 
     func webViewDidFinishLoad(webView : UIWebView) {
         print("Finished!")
-        let requestedURL = myWebView.request?.URL
+        let requestedURL = myWebView.request?.URL?.absoluteString
         print (requestedURL)
+        
+        //Check if the request URL contains our redirect URL
+        // 'lowercaseString' converts both strings to lowercase so it is case agnostic
+        // 'rangeOfString' checks if one string is contained in the other. It returns nil if it does not match.
+        if (requestedURL!.lowercaseString.rangeOfString(redirectURL.lowercaseString) != nil){
+            print("Found your auth code!")
+            let UrlArray = requestedURL!.componentsSeparatedByString("?") //Split the URL string into an array on '?'
+            let queryString = UrlArray[1] // Grab only the things after the '?'
+            let params = parametersFromQueryString(queryString) //We get back a dict of the query params.
+            print(params)
+        }
         
     }
     func webViewDidStartLoad(webView: UIWebView) {
@@ -49,6 +64,40 @@ class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // This function returns a dictionary of the query parameters from the query parmeters in the URL.
+    func parametersFromQueryString(queryString: String?) -> [String: String] {
+        var parameters = [String: String]()
+        if (queryString != nil) {
+            var parameterScanner: NSScanner = NSScanner(string: queryString!)
+            var name:NSString? = nil
+            var value:NSString? = nil
+            while (parameterScanner.atEnd != true) {
+                name = nil;
+                parameterScanner.scanUpToString("=", intoString: &name)
+                parameterScanner.scanString("=", intoString:nil)
+                value = nil
+                parameterScanner.scanUpToString("&", intoString:&value)
+                parameterScanner.scanString("&", intoString:nil)
+                if (name != nil && value != nil) {
+                    parameters[name!.stringByReplacingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!]
+                        = value!.stringByReplacingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+                }
+            }
+        }
+        return parameters
+    }
+    
+    // This function extracts the AUTH CODE from the URL paramters using paramertsFromQueryString()
+    func extractCode(notification: NSNotification) -> String? {
+        let url: NSURL? = (notification.userInfo as!
+            [String: AnyObject])[UIApplicationLaunchOptionsURLKey] as? NSURL
+        
+        // [1] extract the code from the URL
+        return self.parametersFromQueryString(url?.query)["code"]
+    }
+    
     
 
     /*

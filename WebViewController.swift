63c2cc1503a3f5a28b2db3026dc2ba6ed91c9dcc
DanielORorke <dororke@dororke-mac.local>
2016-02-19 10:59:18 -0800

    Installed Pods
    Installed AlamoFire and SwiftyJSON

--- WebViewController.swift
+++ WebViewController.swift
@@ -38,6 +38,7 @@ class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set
     //=============================================================
     }
 
+    // This function is called every time the view finishes loading...
     func webViewDidFinishLoad(webView : UIWebView) {
         print("Finished!")
         let requestedURL = myWebView.request?.URL?.absoluteString
@@ -47,18 +48,25 @@ class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set
         // 'lowercaseString' converts both strings to lowercase so it is case agnostic
         // 'rangeOfString' checks if one string is contained in the other. It returns nil if it does not match.
         if (requestedURL!.lowercaseString.rangeOfString(redirectURL.lowercaseString) != nil){
-            print("Found your auth code!")
+            print ("Spotted the Redirect URL!")
             let UrlArray = requestedURL!.componentsSeparatedByString("?") //Split the URL string into an array on '?'
             let queryString = UrlArray[1] // Grab only the things after the '?'
-            let params = parametersFromQueryString(queryString) //We get back a dict of the query params.
+            let params = parametersFromQueryString(queryString) // We get back a dict of the query params.
             print(params)
+            if params["authCode"] != nil { // Check if the result has authCode
+                print("Found your auth code!")
+                let accessTokensJSON = getAccessTokenFromAuthCode(params)
+            }
+            else { // We don't have the Auth Code
+                // ADD NOTIFICATION TO USER HERE
+                print ("There was an error.")
+            }
         }
         
     }
     func webViewDidStartLoad(webView: UIWebView) {
-        print("Started")
-        let requestedURL = myWebView.request?.URL
-        print (requestedURL)
+        let requestedURL = myWebView.request?.URL?.absoluteString
+        print("STARTED" + requestedURL!)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -88,6 +96,42 @@ class WebViewController: UIViewController, UIWebViewDelegate { //Be sure you set
         }
         return parameters
     }
+    
+    // This function will exchange an AuthCode for an Access Token
+    func getAccessTokenFromAuthCode(params: [String:String]) -> [String: String] {
+        print("Trying to exhange the Auth Code for an Access Token...")
+        let headers = [
+            "X-AH-API-CLIENT-SECRET": clientSecret,
+            "X-AH-API-CLIENT-ID": clientID,
+            "X-AH-API-CLIENT-REDIRECT-URI": redirectURL
+        ]
+        let url = "https://cloud.aerohive.com/services/acct/thirdparty/accesstoken?authCode=" + params["authCode"]! + "&redirectUri=" + redirectURL
+        let requestURL = url
+        // let params = ["ownerID": ownerID] //Use this to better set up query parameters.
+        Alamofire.request(.GET, requestURL, headers: headers /* ,parameters:params */)
+            .responseJSON { response in
+                // Check that the error is nil
+                guard response.result.error == nil else {
+                    // got an error in getting the data, need to handle it
+                    print("error calling GET on "+requestURL)
+                    print(response.result.error!)
+                    print("Headers: ")
+                    print(headers)
+                    return
+                }
+                print("Parsed JSON")
+                if let value: AnyObject = response.result.value {
+                    // handle the results as JSON, without a bunch of nested if loops
+                    let result = JSON(value)
+                }
+                dispatch_async(dispatch_get_main_queue()) {
+                    // Here's where we synch back up with the UI.
+                    
+                }
+        }
+    }
+    
+    
     
     // This function extracts the AUTH CODE from the URL paramters using paramertsFromQueryString()
     func extractCode(notification: NSNotification) -> String? {
